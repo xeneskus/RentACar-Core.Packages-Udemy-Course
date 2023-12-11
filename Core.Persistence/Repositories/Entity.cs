b4f89b9e5df0d332a21f15dd58e760db3e1b519e
xeneskus <xeneskus@gmail.com>
2023-12-11 10:47:33 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public class Entity<TId>
+    {
+        public TId Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }//girmek zorunda degiliz
+        public DateTime? DeletedDate { get; set; } //girmek zorunda degiliz
+        public Entity()
+        {
+            Id = default;//defaultu neyse  o verilsin boşken mesela 0 mı int 0 verilsin
+        }
+        public Entity(TId id) 
+        {
+            Id=id;
+        }
+    }
+}
