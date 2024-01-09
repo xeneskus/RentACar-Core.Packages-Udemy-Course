0f88e5b1ad49c21e6345f77e22b306a61cd8ca29
xeneskus <xeneskus@gmail.com>
2024-01-09 21:15:31 +0300

    Log implementation

---
+++ Core.CrossCuttingConcerns/Logging/LogDetailWithException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Logging;
+
+public class LogDetailWithException : LogDetail
+{
+    public string ExceptionMessage { get; set; }
+    public LogDetailWithException()
+    {
+        ExceptionMessage = string.Empty;
+    }
+    public LogDetailWithException(string fullName, string methodName, string user, List<LogParameter> Parameters, string exceptionMessage)
+    {
+       
+    }
+}
---
+++ Core.CrossCuttingConcerns/Serilog/Logger/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Core.CrossCuttingConcerns.Serilog.Logger;
+
+public class FileLogger : LoggerServiceBase
+{
+    private readonly IConfiguration _configuration;
+
+    public FileLogger(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        FileLogConfiguration logConfig = configuration.GetSection("SerilogLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()// yolu al oradaki alanları bu nesneye maple
+        ?? throw new Exception(SerilogMessages.NullOptionsMessage);// eğer bu alan yoksa
+
+   
+        string logFilePath = string.Format(format: "{0}{1}", arg0: Directory.GetCurrentDirectory() + logConfig.FolderPath, arg1: ".txt");
+
+
+        Logger = new LoggerConfiguration().WriteTo.File(
+            logFilePath, rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: null,
+            fileSizeLimitBytes: 5000000,
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}"
+            ).CreateLogger();
+    }
+
+}
