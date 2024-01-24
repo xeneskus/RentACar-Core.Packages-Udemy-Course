93549cb293e613945f8818cce33506cc6664a763
xeneskus <xeneskus@gmail.com>
2024-01-24 13:51:17 +0300

    Added objects for Role Based Structure

---
+++ Core.Security/Entities/OperationClaim.cs
@@ -0,0 +1,22 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+
+public class OperationClaim : Entity<int>
+{
+    public string Name { get; set; }
+    public virtual ICollection<UserOpertaionClaim> UserOpertaionClaims { get; set; } = null!;
+
+    public OperationClaim()
+    {
+        Name = string.Empty;
+    }
+    public OperationClaim(string name)
+    {
+        Name=name;
+    }
+    public OperationClaim(int id, string name) : base(id)
+    {
+        Name = name;
+    }
+}
---
+++ Core.Security/Entities/UserOpertaionClaim.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+
+public class UserOpertaionClaim:Entity<int>
+{
+    public int UserId { get; set; }
+    public int OperationClaimId { get; set; }
+
+    public virtual User User { get; set; }
+    public virtual OperationClaim OperationClaim { get; set; }
+
+    public UserOpertaionClaim(int userId, int operationClaimId)
+    {
+        UserId = userId;
+        OperationClaimId = operationClaimId;
+    }
+    public UserOpertaionClaim(int id, int userId,int operationClaimId) : base(id)
+    {
+        UserId = userId;
+        OperationClaimId = operationClaimId;
+    }
+}
