66d69e8a56ebc170bf5736c1efe9ca8d2e304d42
xeneskus <xeneskus@gmail.com>
2024-01-26 15:46:52 +0300

    Security Service Registration

---
+++ Core.Security/SecurityServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Security.JWT; 
+using Core.Security.OtpAuthenticator.OtpNet; 
+using Core.Security.OtpAuthenticator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Security
+{
+    public static class SecurityServiceRegistration
+    {
+        public static IServiceCollection AddSecurityServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenHelper, JwtHelper>();
+            //services.AddScoped<IEmailAuthenticatorHelper, EmailAuthenticatorHelper>();
+            services.AddScoped<IOtpAuthenticatorHelper, OtpNetOtpAuthenticatiorHelper>();
+            return services;
+
+        }
+    }
+}
